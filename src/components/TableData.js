5b4d37806cc17b1c663436bed2be5b58aadc66f8
Wutche <wutchechikaome75@gmail.com>
2023-08-13 00:07:10 +0100

    some changes on order table

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -5,17 +5,48 @@ import { Table, Avatar } from "@mantine/core";
 
 const TableData = () => {
 	const elements = [
-		{ id: 1, Date: "01/12/2023", payment: "$12.33", name: "Wutche" },
-		{ id: 2, Date: "11/10/2023", payment: "$56", name: "Sophia" },
-		{ id: 3, Date: "10/3/2023", payment: "$32", name: "Obinna" },
-		{ id: 4, Date: "12/12/2023", payment: "$100", name: "Godrice" },
-		{ id: 5, Date: "05/11/2023", payment: "$35", name: "Henshaw" },
+		{
+			id: 1,
+			name: "Wutche",
+			productName: "Cactus",
+			payment: "$12.33",
+			Date: "01/12/2023",
+		},
+		{
+			id: 2,
+			name: "Sophia",
+			productName: "Cactus",
+			payment: "$12.33",
+			Date: "11/10/2023",
+		},
+		{
+			id: 3,
+			name: "Obinna",
+			productName: "Cactus",
+			payment: "$12.33",
+			Date: "10/3/2023",
+		},
+		{
+			id: 4,
+			name: "Godrice",
+			productName: "Cactus",
+			payment: "$12.33",
+			Date: "12/12/2023",
+		},
+		{
+			id: 5,
+			name: "Henshaw",
+			productName: "Cactus",
+			payment: "$12.33",
+			Date: "05/11/2023",
+		},
 	];
 
 	const rows = elements.map((element) => (
 		<tr key={element.name}>
 			<td>{element.id}</td>
 			<td>{element.name}</td>
+			<td>{element.productName}</td>
 			<td>{element.payment}</td>
 			<td>{element.Date}</td>
 		</tr>
