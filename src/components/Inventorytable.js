dfa842c7d20701806a57318e6dcd764c536c1e56
Sophia Okosodo <sophieokosodo@gmail.com>
2023-08-13 17:06:24 +0100

    add product

--- src/components/Inventorytable.js
+++ src/components/Inventorytable.js
@@ -10,6 +10,8 @@ import instance from "../config/api";
 
 const Inventorytable = () => {
 	const [product, setProduct] = useState([])
+	const [filteredProduct, setFilteredProduct] = useState([])
+	const [filterParam, setFilterParam] = useState("")
 
 	useEffect(() => {
 		instance.get(`/api/v1/user/getAllProducts`, {
@@ -19,10 +21,11 @@ const Inventorytable = () => {
 		}).then((res) => {
 			console.log(res.data.content)
 			setProduct(res.data.content)
+			setFilteredProduct(res.data.content)
 		})
-
+		console.log(filteredProduct)
 		console.log(product)
-	}, [])
+	}, [filterParam])
 
 	const handleEdit = (id) => {
 		Cookies.set("PRODUCT_ID", id)
@@ -30,8 +33,11 @@ const Inventorytable = () => {
 	const handleDelete = (id) => {
 		Cookies.set("PRODUCT_ID", id)
 	}
+
+
+
 	const rows = product.map((element) => (
-		<tr key={element.name}>
+		<tr key={element.productId}>
 			<td>{`#${element.productId}`}</td>
 			<td>{element.productName}</td>
 			<td>
@@ -41,49 +47,46 @@ const Inventorytable = () => {
 			<td>{element.waterRequirement}</td>
 			<td>{element.growthHabit}</td>
 			<td>{element.description}</td>
-			<td>{element.Availability}</td>
+			<td>{element.productPrice}</td>
 			<td>{element.quantity}</td>
-			<td>{element.categoryName}</td>
+			<td>{element.category}</td>
+			<td>{element.productType}</td>
 			<td onClick={() => handleEdit(element.productId)}>
-				<element.inventoryEditICon
-					cursor="pointer"
-					size="20px"
-					color="rgb(11, 102, 11, 1)"
-				/>
+				edit
 			</td>
 			<td onClick={() => handleDelete(element.productId)}>
-				{" "}
-				<element.inventoryDeleteICon
-					cursor="pointer"
-					size="20px"
-					color="rgb(223, 3, 3)"
-				/>{" "}
+				delete
 			</td>
 		</tr>
 	));
 	return <>
 		<div className="inventorytable">
 			<div className="inventoryfilter">
-				<select name="Filter" id="filter">
-					<option value="filterby"> Filter BY </option>
-					<option value="flowering"> Flowering </option>
-					<option value="non-flowering">non-flowering</option>
-					<option value="indoor">indoor</option>
-					<option value="outdoor">outdoor</option>
-					<option value="succulents">succulents</option>
-				</select>
+				{/* <select name="Filter" id="filter">
+					<option value="filterby"  onClick={() => setFilterParam("")}> Filter BY </option>
+					<option value="flowering"  onClick={() => setFilterParam("&filterParam=flowering")}> Flowering </option>
+					<option value="non-flowering" onClick={() => setFilterParam("non-flowering")}>non-flowering</option>
+					<option value="indoor" onClick={() => setFilterParam("indoor")}>indoor</option>
+					<option value="outdoor" onClick={() => setFilterParam("outdoor")}>outdoor</option>
+					<option value="succulents" onClick={() => setFilterParam("&filterParam=flowering")}>succulents</option>
+				</select> */}
 			</div>
 			<Table>
 				<thead>
 					<tr>
-						<th>OrderID</th>
+						<th>Product Id</th>
 						<th>Product Name</th>
-						<th>Image</th>
-						<th>Price</th>
-						<th>Category</th>
-						<th>Date</th>
+						<th>image</th>
+						<th>Light Level</th>
+						<th>Water Requirements</th>
+						<th>Growth Habit</th>
 						<th>Description</th>
-						<th>Availability</th>
+						<th>Product Price</th>
+						<th>Quantity</th>
+						<th>Category</th>
+						<th>Product Type</th>
+						<th></th>
+						<th></th>
 					</tr>
 				</thead>
 				<tbody>{rows}</tbody>
