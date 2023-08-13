ad43a67dcf824eb615ac19aa4ac62cb073444fe0
Wutche <wutchechikaome75@gmail.com>
2023-08-13 10:47:14 +0100

    restructuring

--- src/components/Registereduser.js
+++ src/components/Registereduser.js
@@ -1,9 +1,13 @@
 /** @format */
 
-import React from "react";
-import { Table, Avatar, Pagination, Group } from "@mantine/core";
+import React, { useState, useEffect } from "react";
+import { Table, Avatar, Pagination, Group, Box } from "@mantine/core";
+import { DataTable } from "mantine-datatable";
+import dayjs from "dayjs";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import inventoryport from "../images/portrait.jpg";
+
+const PAGE_SIZES = [10, 15, 20];
 const Registereduser = () => {
 	const elements = [
 		{
@@ -46,6 +50,126 @@ const Registereduser = () => {
 
 			inventoryDeleteICon: RiDeleteBin6Line,
 		},
+		{
+			id: 6,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 7,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 7,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 9,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 10,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 11,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 12,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 13,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 14,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 15,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 16,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 17,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 18,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 19,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
+		{
+			id: 20,
+			Image: inventoryport,
+			User: "Fortune",
+			email: "fortune@gmail.com",
+
+			inventoryDeleteICon: RiDeleteBin6Line,
+		},
 	];
 
 	const rows = elements.map((element) => (
@@ -68,6 +192,15 @@ const Registereduser = () => {
 			</td>
 		</tr>
 	));
+
+	const [pageSize, setPageSize] = useState(PAGE_SIZES[1]);
+
+	useEffect(() => {
+		setPage(1);
+	}, [pageSize]);
+
+	const [page, setPage] = useState(1);
+	const [records, setRecords] = useState(elements.slice(0, pageSize));
 
 	return (
 		<div className="rgstrdusrtbl">
